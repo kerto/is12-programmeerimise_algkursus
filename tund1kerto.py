de04b7137f01fec055fa7d5ecb227c96d22effad
opilane <opilane@PC215-10.(none)>
2013-09-25 11:42:37 +0300

    Kerto

---
+++ tund1kerto.py
@@ -0,0 +1,41 @@
+import random
+from random import randint
+
+klass = ["Kairo","Aigar","Timo","Tiina","Taavi","Toomas","Juhan"] # Massiiv = array
+
+## print len(klass)
+
+## print random.choice(klass) 	## import random - from random import choice
+
+
+## min = 0 
+## max = len(klass) -1
+## n = randint(min,max) 	## import random - from random import randint
+## print klass[n]
+
+## print klass[randint(0, len(klass) -1)]
+
+## del klass[0] 
+
+## print klass
+## n = 0 
+## element = klass[n] 		
+## del klass[n]
+## klass = klass + [element]	
+## print klass
+
+valged = 5
+sinised = 10
+mustad = 15
+
+kuulid = valged + sinised + mustad
+
+print kuulid
+
+
+
+
+
+
+
+
