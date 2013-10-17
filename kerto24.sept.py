04a8033761d04355951e8c9401ba113f02ec0636
opilane <opilane@PC215-10.(none)>
2013-10-17 09:01:36 +0300

    Kodune töö 16 okt

---
+++ kerto24.sept.py
@@ -0,0 +1,27 @@
+import random
+from random import randint
+
+S=5.0
+M=10.0
+V=15.0
+summa= S+M+V
+
+1) suva element
+2) meelde jätta väärtus kuul1
+3) kustutada urnist kuul1
+4) väljasta väärtus
+
+Teine kuul urnist.
+
+Kuul2
+
+
+Ül2. 
+1) masiiv
+2) Võta välja suvaline
+3) lisada tagasi
+
+x100
+
+
+
---
+++ kertokangurkodune.py
@@ -0,0 +1,24 @@
+x = 0
+while x < 100:
+	print '{0:02d} '.format(x),
+	if x==9:
+		print ""
+	if x==19:
+		print ""
+	if x==29:
+		print ""
+	if x==39:
+		print ""
+	if x==49:
+		print ""
+	if x==59:
+		print ""
+	if x==69:
+		print ""
+	if x==79:
+		print ""
+	if x==89:
+		print ""
+	if x==99:
+		print ""
+	x = x + 1
