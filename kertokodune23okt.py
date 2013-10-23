8bd6f2d4ba7c9dd3688fc49a8b0fb2ea1e8b16ce
opilane <opilane@PC215-10.(none)>
2013-10-23 10:43:10 +0300

    tere

--- kertokodune23okt.py
+++ kertokodune23okt.py
@@ -1,9 +1,11 @@
 x=0
+
 while x<100:
-	print "{0:02}".format(x),
+	
+	print "{0:02d}".format(x),
 	x=x+1
 	print "",
 	
-while x%10==9:
-	print "{0:02}".format(x)+ "\n"
-	x=x+1
+if x%10==9:
+	print "{0:02d}".format(x)+ "\n"
+x=x+1
